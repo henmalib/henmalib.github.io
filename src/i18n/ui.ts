f59cf2f214780d47306d703a519c3a5f1db52bc0
Hen <68553709+henmalib@users.noreply.github.com>
2024-11-04 22:38:16 +0100

    Happy birthday in a past

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -10,7 +10,7 @@ export const ui = {
     'meta.description': 'Mini description with contact links',
     title: "I don't know what to write here, but i want a gradient",
     description:
-      "I am 18yo self-taught programmer from the Luhansk region of Ukraine. I like listening to <a class='text-mauve hover:animate-pulse' href='https://en.wikipedia.org/wiki/Breakcore'>breakcore</a>, playing story games, and developing small useful utilities for myself.",
+      "I am 19yo self-taught programmer from the Luhansk region of Ukraine. I like listening to <a class='text-mauve hover:animate-pulse' href='https://en.wikipedia.org/wiki/Breakcore'>breakcore</a>, playing story games, and developing small useful utilities for myself.",
     prevKnown:
       'Also i was previously known by someone as <spam class="text-mauve">@simidzu2ay</span>',
     connect: 'You can find me on the links below',
@@ -20,7 +20,7 @@ export const ui = {
     'meta.description': "Міні-опис з посиланнями зв'язку",
     title: 'Я не знаю що тут писати, але хочу градієнт',
     description:
-      "Я - 18 річний самоучка-програміст з Луганської області в Україні. Я люблю слухати <a class='text-mauve hover:animate-pulse' href='https://uk.wikipedia.org/wiki/%D0%91%D1%80%D0%B5%D0%B9%D0%BA%D0%BA%D0%BE%D1%80'>breakcore</a>, грати в сюжетні ігри та розробляти маленькі корисні утіліти для себе",
+      "Я - 19 річний самоучка-програміст з Луганської області в Україні. Я люблю слухати <a class='text-mauve hover:animate-pulse' href='https://uk.wikipedia.org/wiki/%D0%91%D1%80%D0%B5%D0%B9%D0%BA%D0%BA%D0%BE%D1%80'>breakcore</a>, грати в сюжетні ігри та розробляти маленькі корисні утіліти для себе",
     prevKnown:
       'Деякі мене знають як <span class="text-mauve">@simidzu2ay</span>',
     connect: 'Ви можете знайти в будь-де в вказаних посиланнях',

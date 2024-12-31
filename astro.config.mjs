14a15e6f58735ff768757825bcb6c3a738ce1aac
simidzu2ay <simidzu2ay@protonmail.com>
2024-12-31 17:30:35 +0100

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,23 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()],
-  site: "https://henmalib.github.io",
+	integrations: [
+		tailwind(),
+		icon(),
+		sitemap({
+			i18n: {
+				defaultLocale: "en",
+				locales: {
+					en: "en",
+					uk: "uk",
+				},
+			},
+		}),
+	],
+	site: "https://henmalib.dev",
 });
